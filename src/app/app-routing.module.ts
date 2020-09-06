456c73fb3d78e9726a9449b4cfbd7de3083c41bf
Johan Millers <Johanoscarmarte@gmail.com>
2020-09-06 17:12:50 -0400

    Integracion de Servicio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+const routes: Routes = [];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { SearchComponent } from './Componentes/search/search.component';
+import { NavbarComponent } from './Componentes/shared/navbar/navbar.component';
+import { CallbackComponent } from './Componentes/callback/callback.component';
+import { AuthService } from './Service/auth.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    CallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
