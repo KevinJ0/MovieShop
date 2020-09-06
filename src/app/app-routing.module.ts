90cf9a5da2b4822da14017cb85fbb725a0c503f6
Johan Millers <Johanoscarmarte@gmail.com>
2020-09-06 17:47:50 -0400

    integrando login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './Componentes/home/home.component';
+import { SearchComponent } from './Componentes/search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+];
 
 
 
